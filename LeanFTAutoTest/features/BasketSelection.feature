55cb8a5254a79a36bf3497851f63dbf0eac30e5e
edgewords <info@edgewords.co.uk>
2018-04-25 15:34:44 +0100

    Add project files.

---
+++ LeanFTAutoTest/features/BasketSelection.feature
@@ -0,0 +1,10 @@
+﻿#Auto generated Octane revision tag
+@TID1001REV0.2.0
+Feature: BasketSelection
+	As a customer
+	I want to be able to add products to my basket
+
+Scenario: Adding Item to Basket
+	Given I am on the shop web site
+	When I add an item to the basket
+	Then I can see the item in the basket
---
+++ LeanFTAutoTest/step_bindings/BasketSelectionSteps.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using System.Diagnostics;
+using HP.LFT.SDK;
+using HP.LFT.SDK.Web;
+using NUnit.Framework;
+
+namespace LeanFTAutoTest.step_bindings
+{
+    [Binding]
+    public class BasketSelectionSteps : UnitTestClassBase
+    {
+        IBrowser browser;
+
+        [Given(@"I am on the shop web site")]
+        public void GivenIAmOnTheShopWebSite()
+        {
+            browser = BrowserFactory.Launch(BrowserType.Chrome);
+            browser.Navigate("https://www.edgewordstraining.co.uk/demo-site");
+        }
+        
+        [When(@"I add an item to the basket")]
+        public void WhenIAddAnItemToTheBasket()
+        {
+            var sEditField = browser.Describe<IEditField>(new EditFieldDescription
+            {
+                AccessibilityName = string.Empty,
+                Name = @"s",
+                Placeholder = @"Search products…",
+                TagName = @"INPUT",
+                Type = @"search",
+                Index = 0
+            });
+            sEditField.SetValue("cap");
+
+            var searchButton = browser.Describe<IButton>(new ButtonDescription
+            {
+                AccessibilityName = string.Empty,
+                ButtonType = @"submit",
+                Name = @"Search",
+                Role = string.Empty,
+                TagName = @"BUTTON",
+                Index = 0
+            });
+            searchButton.Click();
+
+            var addToCartButton = browser.Describe<IButton>(new ButtonDescription
+            {
+                ButtonType = @"submit",
+                Name = @"Add to cart",
+                TagName = @"BUTTON"
+            });
+            addToCartButton.Click();
+
+            var homeLink = browser.Describe<ILink>(new LinkDescription
+            {
+                AccessibilityName = string.Empty,
+                InnerText = @"Home",
+                Role = string.Empty,
+                TagName = @"A",
+                Index = 0
+            });
+            homeLink.Click();
+
+            var cartLink = browser.Describe<ILink>(new LinkDescription
+            {
+                AccessibilityName = string.Empty,
+                InnerText = @"Cart",
+                Role = string.Empty,
+                TagName = @"A",
+                Index = 0
+            });
+            cartLink.Click();
+        }
+        
+        [Then(@"I can see the item in the basket")]
+        public void ThenICanSeeTheItemInTheBasket()
+        {
+            // check cap exists in basket
+            var capLink = browser.Describe<ILink>(new LinkDescription
+            {
+                InnerText = @"Cap",
+                TagName = @"A"
+            });
+            Assert.That(capLink.IsVisible);
+
+            var removeThisItemLink = browser.Describe<ILink>(new LinkDescription
+            {
+                InnerText = @"×",
+                TagName = @"A"
+            });
+            removeThisItemLink.Click();
+
+            var returnToShopLink = browser.Describe<ILink>(new LinkDescription
+            {
+                InnerText = @"Return to shop ",
+                TagName = @"A"
+            });
+            returnToShopLink.Click();
+
+            browser.CloseAllTabs();
+        }
+    }
+}
